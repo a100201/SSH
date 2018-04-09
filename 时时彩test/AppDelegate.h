c02a1d0b5ed2d3c9e607c04ce6b4ec8f1103ec2d
forklyMerry <1668446724@qq.com>
2018-04-09 11:07:46 +0800

    Initial Commit

---
+++ 时时彩test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  时时彩test
+//
+//  Created by fly on 2018/4/9.
+//  Copyright © 2018年 fly111. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
